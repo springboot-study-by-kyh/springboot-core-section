bac4bb0bae51447bf4d50b3bd5c322ec484a616f
sjh9391985 <sjh9391985@gmail.com>
2023-10-12 22:30:51 +0900

    feat: "주문 테스트 코드"
    - 주문 생성

--- src/test/java/com/example/coresectionone/order/OrderServiceTest.java
+++ src/test/java/com/example/coresectionone/order/OrderServiceTest.java
@@ -1,28 +1,25 @@
-package com.example.coresectionone;
+package com.example.coresectionone.order;
 
 import com.example.coresectionone.member.Grade;
 import com.example.coresectionone.member.Member;
 import com.example.coresectionone.member.MemberService;
 import com.example.coresectionone.member.MemberServiceImpl;
-import com.example.coresectionone.order.Order;
-import com.example.coresectionone.order.OrderService;
-import com.example.coresectionone.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class OrderApp {
-    public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+public class OrderServiceTest {
 
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
         Long memberId = 1L;
         Member memberA = new Member(memberId, "memberA", Grade.VIP);
-
         memberService.join(memberA);
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
-
-        System.out.println(order);
-
-
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 
 }
