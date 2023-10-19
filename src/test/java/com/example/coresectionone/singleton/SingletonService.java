1c917651602a1ce6e35ffa6671b5c81c8885ae4b
sjh9391985 <sjh9391985@gmail.com>
2023-10-19 20:55:50 +0900

    feat: "싱글톤 패턴 테스트"

---
+++ src/test/java/com/example/coresectionone/singleton/SingletonService.java
@@ -0,0 +1,21 @@
+package com.example.coresectionone.singleton;
+
+public class SingletonService {
+
+    // static 영역에 객체를 딱 1개만 생성해둠
+    private static final SingletonService instance = new SingletonService();
+
+    // public으로 열어서 객체 인스턴스가 필요시 static 메서드를 통해서만 조회하도록 허용함
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    // 생성자를 private로 선언해서 외부에서 new 키워드를 사용한 객체 생성을 못하게 막음.
+    private SingletonService(){}
+
+
+    public void login(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+
+}
