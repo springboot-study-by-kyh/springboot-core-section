2ef8fa751c0001d12fb0a7b9719c30215858d7b2
sjh9391985 <sjh9391985@gmail.com>
2023-10-22 10:28:31 +0900

    feat: "스프링 싱글톤 방식"

--- src/test/java/com/example/coresectionone/singleton/SingletonTest.java
+++ src/test/java/com/example/coresectionone/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import com.example.coresectionone.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -40,4 +42,21 @@ public class SingletonTest {
 
     }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1 . 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2. 호출할 때 마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값 확인
+        System.out.println("memberService1 : " + memberService1);
+        System.out.println("memberService2 : " + memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+    }
 }
