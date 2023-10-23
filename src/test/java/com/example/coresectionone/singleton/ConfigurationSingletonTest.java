fd8cdf5eaf1f81392e34316867831a36c61a7df1
sjh9391985 <sjh9391985@gmail.com>
2023-10-23 20:10:27 +0900

    feat: "싱글톤 호출"

---
+++ src/test/java/com/example/coresectionone/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,25 @@
+package com.example.coresectionone.singleton;
+
+import com.example.coresectionone.AppConfig;
+import com.example.coresectionone.member.MemberRepository;
+import com.example.coresectionone.member.MemberServiceImpl;
+import com.example.coresectionone.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        Assertions.assertThat(memberRepository1).isEqualTo(memberRepository2);
+    }
+
+}
