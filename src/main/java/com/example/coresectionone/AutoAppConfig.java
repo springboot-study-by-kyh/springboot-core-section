21715fd9756241d8154fd601398811c761313751
sjh9391985 <sjh9391985@gmail.com>
2023-10-25 22:07:40 +0900

    feat: "컴포넌트 스캔 설명"

--- src/main/java/com/example/coresectionone/AutoAppConfig.java
+++ src/main/java/com/example/coresectionone/AutoAppConfig.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(
+@ComponentScan( // @Component 가 붙은 모든 클래스를 스프링 빈으로 등록
     excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 
-    
+
 
 
 }
