ee9b3c6cd7840b035d40c655d105e0ae6486b171
sjh9391985 <sjh9391985@gmail.com>
2023-10-27 11:08:50 +0900

    feat: "컴포넌트 스캔 시작 패키지 지정"

--- src/main/java/com/example/coresectionone/AutoAppConfig.java
+++ src/main/java/com/example/coresectionone/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( // @Component 가 붙은 모든 클래스를 스프링 빈으로 등록
+    basePackages = "com.example.coresectionone.member", // 탐색할 시작 패키지 지정
+//    basePackageClasses = AutoAppConfig.class,
     excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
