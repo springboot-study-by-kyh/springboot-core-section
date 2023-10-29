d0e1b95680196394a0ca8168a35302ba586540f4
sjh9391985 <sjh9391985@gmail.com>
2023-10-29 14:35:05 +0900

    feat: "Lombok 추가"

--- src/main/java/com/example/coresectionone/order/OrderServiceImpl.java
+++ src/main/java/com/example/coresectionone/order/OrderServiceImpl.java
@@ -3,20 +3,16 @@ package com.example.coresectionone.order;
 import com.example.coresectionone.discount.DiscountPolicy;
 import com.example.coresectionone.member.Member;
 import com.example.coresectionone.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; // 구체화에 의존하지 않고 추상화인 인터페이스에만 의존을 함
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
