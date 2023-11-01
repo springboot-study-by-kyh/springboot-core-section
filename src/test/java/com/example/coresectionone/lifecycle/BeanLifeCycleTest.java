bf346c7e0abfc0a6ca503799f1f45ee23a76db61
sjh9391985 <sjh9391985@gmail.com>
2023-11-01 20:48:33 +0900

    feat: "빈 생명주기"

---
+++ src/test/java/com/example/coresectionone/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package com.example.coresectionone.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 스프링 빈 라이프사이클: 객체 생성 -> 의존관계 주입
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+
+}
