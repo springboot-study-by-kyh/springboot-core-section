a2cb4721b48480829c717cd983cd59cf9603107e
sjh9391985 <sjh9391985@gmail.com>
2023-11-05 20:15:05 +0900

    feat: "scope provider"

--- src/main/java/com/example/coresectionone/web/LogDemoController.java
+++ src/main/java/com/example/coresectionone/web/LogDemoController.java
@@ -3,6 +3,7 @@ package com.example.coresectionone.web;
 import com.example.coresectionone.common.MyLogger;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,16 +13,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");
-
         return "OK";
     }
 }
