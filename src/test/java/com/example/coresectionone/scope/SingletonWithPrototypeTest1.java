9bbda1562b20bb405e419afff92ad8b1dfff96be
sjh9391985 <sjh9391985@gmail.com>
2023-11-05 15:19:17 +0900

    feat: "싱글톤 사용전 프로토타입 스코프"

---
+++ src/test/java/com/example/coresectionone/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,45 @@
+package com.example.coresectionone.scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean bean = ac.getBean(PrototypeBean.class);
+        bean.addCount();
+        Assertions.assertThat(bean.getCount()).isEqualTo(1);
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("prototypeBean.init = " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("prototypeBean.destroy");
+        }
+
+    }
+
+}
